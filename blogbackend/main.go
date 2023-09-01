3f4783052ffc1d17979a0b240806c69e77eb73ba
Gautam <gautamvijayarora@gmail.com>
2023-09-01 20:22:17 +0530

    backend updated

--- blogbackend/main.go
+++ blogbackend/main.go
@@ -7,6 +7,7 @@ import (
 	"github.com/gautamarora04/database"
 	"github.com/gautamarora04/routes"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/joho/godotenv"
 )
 
@@ -18,7 +19,15 @@ func main() {
 	}
 
 	port := os.Getenv("PORT")
+	// CORS middleware configuration
+
 	app := fiber.New()
+	app.Use(cors.New(cors.Config{
+		AllowOrigins:     "http://localhost:3000", // Replace with your frontend URL(s)
+		AllowMethods:     "GET,POST,PUT,DELETE,USE,STATIC",
+		AllowHeaders:     "Authorization,Content-Type",
+		AllowCredentials: true, // Allow credentials (cookies, HTTP authentication) to be sent with the request
+	}))
 	routes.Serve(app)
 	app.Listen(":" + port)
 
